469c2fea13cbc2b4c6606820549dcfa09d04fb00
Arianna Perez <akdev.aring@gmail.com>
2023-09-29 16:25:43 +0800

    [React-01] Add filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,16 +8,25 @@ const Expenses = (props) => {
   const expenses = props.expenses
   const [filteredYear, setFilteredYear] = useState('2020');
 
+  
+  // let filterInfoText = '2019, 2021, & 2022';
+
+  // if (filteredYear === '2019') {
+  //   filterInfoText = '2020, 2021, & 2022';
+  // }  else if (filteredYear === '2021') {
+  //   filterInfoText = '2019, 2020, & 2022';
+  // } else {
+  //   filterInfoText = '2019, 2020, & 2021';
+  // }
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
    
   return (
-    <div className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}>
-        </ExpensesFilter>
-      <Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
         <ExpenseItem 
           title={expenses[0].title}
           amount={expenses[0].amount}
